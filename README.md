0462a1fe87f3433ad8dbf6b30fcce2c481a15554
Lensco825 <lensdorsainvil1@gmail.com>
2025-06-21 18:04:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daily-quote-website
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Lensco825/daily-quote-website)
---
+++ script.js
@@ -0,0 +1,6 @@
+const quoteHeading = document.querySelector('h1');
+const quoteAuthor = document.querySelector('p');
+
+// fetch('https://api.animechan.io/v1/quotes/random')
+//     .then(response => response.json())
+//     .then(quote => console.log(quote));
