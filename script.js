fe067d38afaee927c33b06aa9d703c9d4bb531c8
Lens <lensdorsainvil711@outlook.com>
2025-06-22 13:14:39 -0400

    Successfully connected the data from the API onto the innerHTML of the website, quotes are now visible on the website

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const quoteHeading = document.querySelector('h1');
 const quoteAuthor = document.querySelector('p');
 
-// fetch('https://api.animechan.io/v1/quotes/random')
-//     .then(response => response.json())
-//     .then(quote => console.log(quote));
+fetch('https://api.animechan.io/v1/quotes/random')
+    .then(response => response.json())
+    .then(quote => {
+        quoteHeading.innerHTML = quote.data.content
+        quoteAuthor.innerHTML = `${quote.data.character.name} from ${quote.data.anime.name}`;
+        }
+        );
+
+
+
